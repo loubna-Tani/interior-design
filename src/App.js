e408e0ffc4b4648a462e872163c05d986b695ffc
loubna-tani <49785408+loubnaben@users.noreply.github.com>
2020-08-19 19:47:12 -0500

    First commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+import Home from "./pages/Home";
+import Design from "./pages/Design";
+import Lifestyle from "./pages/LifeStyle";
+import ShoppingGuides from "./pages/ShoppingGuides";
+
+function App() {
+  return <></>;
+}
+
+export default App;
---
+++ src/pages/ShoppingGuides.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function ShoppingGuides() {
+  return <div></div>;
+}
